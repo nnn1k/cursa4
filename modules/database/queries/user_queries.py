9b03d29fc0f7ffc3a882761d8d5ff92d80461bb9
nikit <nikitos.elidu@gmail.com>
2024-10-23 12:11:59 +0300

    refactoring code

--- modules/database/queries/user_queries.py
+++ modules/database/queries/user_queries.py
@@ -1,31 +1,27 @@
 from modules.database.db_class import db
 from modules.models.User import User
-
-def create_user_object(res):
-    if not res['error'] and res['result']:
-        return User(*res['result'][0])
-    else:
-        return None
+from modules.func.utils import *
 
 def auth_user(login, password) -> User:
-    res = db.execute_query('select * from users where login = ? and password = ?', login, password, is_select=True)
-    return create_user_object(res)
+    res = db.execute_query('select * from users where login = ? and password = ?', login, password, is_select=True)['result'][0]
+    print(res)
+    return create_cls_object(res, User)
 
 def add_user(login, password) -> User:
     db.execute_query('insert into users (login, password) values (?, ?)', login, password)
     return get_user_for_login(login)
 
 def get_user_for_login(login) -> User:
-    res = db.execute_query('select * from users where login = ?', login, is_select=True)
-    return create_user_object(res)
+    res = db.execute_query('select * from users where login = ?', login, is_select=True)['result'][0]
+    return create_cls_object(res, User)
 
 def get_user_for_id(user_id) -> User:
-    res = db.execute_query('select * from users where id = ?', user_id,  is_select=True)
-    return create_user_object(res)
+    res = db.execute_query('select * from users where id = ?', user_id,  is_select=True)['result'][0]
+    return create_cls_object(res, User)
 
 def get_user_for_email(email) -> User:
-    res = db.execute_query('select * from users where email = ?', email,  is_select=True)
-    return create_user_object(res)
+    res = db.execute_query('select * from users where email = ?', email,  is_select=True)['result'][0]
+    return create_cls_object(res, User)
 
 def filling_form(user_id, name, surname, email, phone, photo_url) -> User:
     db.execute_query('''
